accd17718b56b51a8129504f18c8407e43c14c0f
Rubin Xu <rubinxu@google.com>
2018-01-11 10:59:19 +0000

    Add permission check to setAllowOnlyVpnForUids
    Bug: 63000005
    Test: runtest frameworks-net -c com.android.server.connectivity.VpnTest
    Test: cts-tradefed run cts-dev -m CtsDevicePolicyManagerTestCases -t com.android.cts.devicepolicy.MixedDeviceOwnerTest#testAlwaysOnVpnLockDown
    Merged-In: Ia1a82ee73d8617f3124032986fe6c09c14bf7752
    Change-Id: Ia1a82ee73d8617f3124032986fe6c09c14bf7752
    (cherry picked from commit f915e04d5010c4dfffad263fa70c1e412e856314)
    CVE-2017-13314

--- services/core/java/com/android/server/NetworkManagementService.java
+++ services/core/java/com/android/server/NetworkManagementService.java
@@ -1917,6 +1917,8 @@ public class NetworkManagementService extends INetworkManagementService.Stub
     @Override
     public void setAllowOnlyVpnForUids(boolean add, UidRange[] uidRanges)
             throws ServiceSpecificException {
+        mContext.enforceCallingOrSelfPermission(android.Manifest.permission.NETWORK_STACK, TAG);
+
         try {
             mNetdService.networkRejectNonSecureVpn(add, uidRanges);
         } catch (ServiceSpecificException e) {
